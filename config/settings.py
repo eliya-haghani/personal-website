b81e335f4e3157eca03b4bcc020b09e67276cafb
iliahaghani <iliahaghani05@gmail.com>
2026-06-10 11:38:20 +0330

    fix: move SECRET_KEY to env file

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 from pathlib import Path
 
@@ -20,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4%m3t)-(9m)5t*a8)-^s==8cvex@hsk^n5m*j6^k22^s18luq)'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['personal-website-production-828e.up.railway.app', 'localhost', '127.0.0.1']
 
